870f7cf454a44875525d844b7cfbafea6142bd26
sonofsafwat <abdelrhman.safwat@gmail.com>
2020-08-20 15:00:15 +0400

    update post meta tag to include author plus date

--- src/components/Post/Meta/Meta.js
+++ src/components/Post/Meta/Meta.js
@@ -2,15 +2,29 @@
 import React from 'react';
 import moment from 'moment';
 import styles from './Meta.module.scss';
+import { getContactHref } from '../../../utils';
+import { useSiteMetadata } from '../../../hooks';
 
 type Props = {
-  date: string
+	date: string,
 };
 
-const Meta = ({ date }: Props) => (
-  <div className={styles['meta']}>
-    <p className={styles['meta__date']}>Published {moment(date).format('D MMM YYYY')}</p>
-  </div>
-);
+const Meta = ({ date }: Props) => {
+	const { author } = useSiteMetadata();
+	return (
+		<div className={styles['meta']}>
+			<p className={styles['meta__date']}>
+				Published {moment(date).format('D MMM YYYY')} By&nbsp;
+				<a
+					className={styles['meta__date-twitter']}
+					href={getContactHref('twitter', author.contacts.twitter)}
+					rel='noopener noreferrer'
+					target='_blank'>
+					<strong>{author.name}</strong>
+				</a>
+			</p>
+		</div>
+	);
+};
 
 export default Meta;
